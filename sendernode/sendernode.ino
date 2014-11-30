ef7069f072b9191a256dfa05318a64debf01a6b4
vfarcy <vfarcy@free.fr>
2014-11-30 21:28:21 +0100

    updates

--- sendernode/sendernode.ino
+++ sendernode/sendernode.ino
@@ -16,7 +16,7 @@
 #define FREQUENCY   RF69_868MHZ
 //#define FREQUENCY     RF69_915MHZ
 #define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
-//#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
+#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
 #define ACK_TIME      30 // max # of ms to wait for an ack
 #ifdef __AVR_ATmega1284P__
   #define LED           15 // Moteino MEGAs have LEDs on D15
